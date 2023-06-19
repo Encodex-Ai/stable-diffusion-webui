d9171d88c28fc06886c66bf73680cded69c7eb9b
Cameron Faith <127695511+encodexdev@users.noreply.github.com>
2023-06-19 13:35:46 -0600

    Latest valid settings

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON="C:\Users\cmrnf\AppData\Local\Programs\Python\Python310\python.exe"
+set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--xformers
 
 call webui.bat
