8a51a613f7fa8cb4c8549fa17573c5d1ecae92ba
Cameron Faith <127695511+encodexdev@users.noreply.github.com>
2023-07-04 11:03:40 -0600

    Add API flag to startup params

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--xformers --opt-sdp-attention --disable-nan-check
+set COMMANDLINE_ARGS=--xformers --opt-sdp-attention --disable-nan-check --api
 
 call webui.bat
